72599c0fc258f56cc3e2d9e08df59eab9d19e480
nrooney024 <nrooney024@gmail.com>
2022-07-15 16:50:56 -0400

    added get and database connection

--- server.js
+++ server.js
@@ -9,11 +9,29 @@ PORT = 80
 
 let db,
     dbConnectionString = process.env.DB_STRING,
-    dbName = 'sample_mflix',
+    dbName = 'five-zero-one-society',
     collection
 
+MongoClient.connect(dbConnectionString)
+    .then(client => {
+        console.log(`Connected to Database`)
+        db = client.db(dbName)
+        collection = db.collection('five-zero-one-society')
+    })
 
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(cors())
+
+app.get('/', async (request, response) => {
+    try {
+        response.render('index.ejs')
+    } catch (error) {
+        response.status(500).send({message: error.message})
+    }
+})
 
 app.listen(process.env.PORT || PORT, function() {
     console.log(`Listening...`)
-})
