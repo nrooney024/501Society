26ca42a26bdb6ffd0842afbdbff01c11fbc8a7b6
nrooney024 <nrooney024@gmail.com>
2022-08-25 22:06:19 -0400

    updating routes to '/'

--- routes/learning-stacks-routes.js
+++ routes/learning-stacks-routes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const learningStacksControllers = require('../controllers/learning-stacks-controllers')
 
-router.get('/learning-stacks', learningStacksControllers.getLearningStacks)
+router.get('/', learningStacksControllers.getLearningStacks)
 
 router.post('/createLearningStack', learningStacksControllers.createLearningStack)
 
