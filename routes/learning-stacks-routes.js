188c8ab65a1d1d14ca99e6fada05cc816adbfd78
nrooney024 <nrooney024@gmail.com>
2022-09-18 18:49:21 -0400

    Adding ensure auth to learning stacks

--- routes/learning-stacks-routes.js
+++ routes/learning-stacks-routes.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const learningStacksControllers = require('../controllers/learning-stacks-controllers')
+const { ensureAuth } = require('../middleware/auth')
 
-router.get('/', learningStacksControllers.getLearningStacks)
+router.get('/', ensureAuth, learningStacksControllers.getLearningStacks)
 
 router.post('/createLearningStack', learningStacksControllers.createLearningStack)
 
