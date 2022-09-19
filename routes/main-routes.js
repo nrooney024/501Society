acd16ce678eecf06b8bdbf8b63d2938705707e43
nrooney024 <nrooney024@gmail.com>
2022-09-19 19:24:15 -0400

    Updating dependencies and uncommented out auth routes

--- routes/main-routes.js
+++ routes/main-routes.js
@@ -4,10 +4,10 @@ const mainController = require('../controllers/main-controllers')
 const authController = require('../controllers/auth')
 
 router.get('/', mainController.getIndex) 
-// router.get('/login', authController.getLogin)
-// router.post('/login', authController.postLogin)
-// router.get('/logout', authController.logout)
-// router.get('/signup', authController.getSignup)
-// router.post('/signup', authController.postSignup)
+router.get('/login', authController.getLogin)
+router.post('/login', authController.postLogin)
+router.get('/logout', authController.logout)
+router.get('/signup', authController.getSignup)
+router.post('/signup', authController.postSignup)
 
 module.exports = router
