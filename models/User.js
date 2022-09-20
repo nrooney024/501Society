8810d9df3f020f3be8285c2ec53f527c764c9729
nrooney024 <nrooney024@gmail.com>
2022-09-19 21:48:12 -0400

    Updating User schema

--- models/User.js
+++ models/User.js
@@ -2,10 +2,30 @@ const bcrypt = require('bcrypt')
 const mongoose = require('mongoose')
 
 const UserSchema = new mongoose.Schema({
-  userName: { type: String, unique: true },
-  email: { type: String, unique: true },
-  password: String
+  userName: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  learningStackArray: {
+    type: [LearningStackSchema],
+    required: true
+  },
+  uniqueDatesLoggedIn: {
+    type: [String],
+    required: true,
+  }
 })
+
 
 
 // Password hash middleware.
