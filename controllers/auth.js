38c55e845e2b6e9be74794a676e496ddd6fea8dc
nrooney024 <nrooney024@gmail.com>
2022-09-19 22:23:14 -0400

    Fixing logout

--- controllers/auth.js
+++ controllers/auth.js
@@ -38,18 +38,14 @@ const User = require('../models/User')
   
   
 
+  
   exports.logout = (req, res) => {
-    console.log(`Checkpoint 1: ${req.session}`)
-    req.logout(() => {
-      console.log('User has logged out.')
-    })
     req.session.destroy((err) => {
-      if (err) console.log('Error : Failed to destroy the session during logout.', err)
       req.user = null
-      console.log(`Checkpoint 2: ${req.session}`)
       res.redirect('/')
     })
   }
+  
   
   exports.getSignup = (req, res) => {
     if (req.user) {
