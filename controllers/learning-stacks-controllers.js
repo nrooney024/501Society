eb75b3c64a115fcd92a1da48811bc5347970fade
nrooney024 <nrooney024@gmail.com>
2022-09-24 00:27:10 -0400

    Adding id references in models, updating controllers to reflect this, and updating ejs learning stack header

--- controllers/learning-stacks-controllers.js
+++ controllers/learning-stacks-controllers.js
@@ -1,12 +1,16 @@
-const LearningStack = require('../models/Learning-stacks-models')
+const {LearningStackExport, LearningResourcesExport} = require('../models/Learning-stacks-models')
 const User = require('../models/User')
 
 
 module.exports = {
     getLearningStacks: async (req,res)=>{
         try{
-            const user = await User.findById(req.user.id);
+            const user = await User.findById(req.user.id).populate('learningStackArray');
+            console.log(`*******user: ${user}`)
+            
             const learningStackArray = user.learningStackArray
+            console.log(`*******learningStackArray: ${learningStackArray}`)
+
             res.render('learning-stacks.ejs', {learningStacks: learningStackArray})
         }catch(err){ 
             console.log(err)
@@ -17,14 +21,25 @@ module.exports = {
             // User signed in
             const user = await User.findById(req.user.id);
             
-            // Learning stack array of the user signed in
-            const learningStackArray = user.learningStackArray
-            
-            // Adding new learning stack from form
-            learningStackArray.push({
+            const learningStack = await LearningStackExport.create({
                 learningStackName: req.body.learningStackName,
                 learningResourcesList: []
             })
+
+            console.log(`******learningStack: ${learningStack}`)
+            
+            // Adding new learning stack from form
+            // learningStackArray.push({
+            //     learningStackName: req.body.learningStackName,
+            //     learningResourcesList: []
+            // })
+
+
+              // Learning stack array of the user signed in
+              const learningStackArray = user.learningStackArray
+
+              learningStackArray.push(learningStack.id)
+
             console.log('Learning stack has been added!')
             res.redirect('/learning-stacks')
             user.save()
