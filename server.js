2958d669dd332bdb207a473cadff42e98839e7b9
nrooney024 <nrooney024@gmail.com>
2022-11-08 22:22:22 -0500

    Reverted mongostore changes

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use(
       resave: false,
       saveUninitialized: false,
       store: MongoStore.create({
-        mongoUrl: mongoose.connection.getClient()
+        client: mongoose.connection.getClient()
       }),
     })
   )
