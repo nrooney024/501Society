296ff2acea93677a12902ce40494f47389d84a20
nrooney024 <nrooney024@gmail.com>
2023-02-06 20:52:15 -0500

    Added Google details to User schema

--- models/User.js
+++ models/User.js
@@ -3,6 +3,29 @@ const mongoose = require('mongoose')
 const LearningStackSchema = require('./Learning-stacks-models').schema
 
 const UserSchema = new mongoose.Schema({
+  googleId: {
+    type: String,
+    required: true,
+  },
+  displayName: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
   userName: {
     type: String,
     unique: true,
