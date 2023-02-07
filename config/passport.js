b34ab5604db29af6f34847ed536803347b7c3612
nrooney024 <nrooney024@gmail.com>
2023-02-06 20:47:56 -0500

    Added google strategy to passport.js

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,7 @@
 const LocalStrategy = require('passport-local').Strategy
 const mongoose = require('mongoose')
 const User = require('../models/User')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
 
 module.exports = function (passport) {
   passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
