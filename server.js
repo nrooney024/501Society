ab6144d7462887d7f12690c92065dba628b2ba33
nrooney024 <nrooney024@gmail.com>
2023-02-08 21:10:27 -0500

    Fixed public folder path

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
@@ -24,11 +25,14 @@ require('./config/passport')(passport)
 connectDB()
 
 app.set('view engine', 'ejs')
-app.use(express.static('public'))
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(cors())
 app.use(logger('dev'))
+
+// Static folder
+app.use(express.static(path.join(__dirname, 'public')))
+
 
 //Use forms for put / delete
 app.use(methodOverride("_method"));
