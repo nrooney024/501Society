a92046555ca63208390591ed79ca46f41c4ae710
nrooney024 <nrooney024@gmail.com>
2023-02-08 22:32:30 -0500

    Fixing auth routes

--- routes/main-routes.js
+++ routes/main-routes.js
@@ -2,13 +2,13 @@ const express = require('express')
 const router = express.Router()
 const mainController = require('../controllers/main-controllers')
 const authController = require('../controllers/auth')
-
+const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
 router.get('/', mainController.getIndex) 
-router.get('/login', authController.getLogin)
+router.get('/login', ensureGuest, authController.getLogin)
 router.post('/login', authController.postLogin)
 router.get('/logout', authController.logout)
-router.get('/signup', authController.getSignup)
+router.get('/signup', ensureGuest, authController.getSignup)
 router.post('/signup', authController.postSignup)
 
 module.exports = router
