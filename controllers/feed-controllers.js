0e811686498c3feb50f6fa26f2a385501d434683
nrooney024 <nrooney024@gmail.com>
2023-02-25 11:16:59 -0500

    Starting to put together allLearningStacks and allUserImages in feed controller

--- controllers/feed-controllers.js
+++ controllers/feed-controllers.js
@@ -1,12 +1,22 @@
 const {LearningStackExport, LearningResourcesExport} = require('../models/Learning-stacks-models')
+const { db } = require('../models/User')
 const User = require('../models/User')
 
 module.exports = {
     getFeed: async (req,res)=>{
         try{
-            
-            res.render('feed.ejs')
+            const allLearningStacks = []
+            const allUserImages = []
 
+            db.users.forEach(user =>{
+                allLearningStacks.push(user.populate('learningStackArray'))
+                allUserImages.push(user.userImage)
+            })
+
+            res.render('feed.ejs', {
+                allLearningStacks: allLearningStacks,
+                allUserImages: allUserImages
+            })
         }catch(err){ 
             console.log(err)
         }
