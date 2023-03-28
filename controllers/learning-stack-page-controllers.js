2a463a19148473e55581cc7d654713b8821b1e8d
nrooney024 <nrooney024@gmail.com>
2023-03-27 22:03:29 -0400

    Update learning-stack-page-controllers.js

--- controllers/learning-stack-page-controllers.js
+++ controllers/learning-stack-page-controllers.js
@@ -1,5 +1,6 @@
 const {LearningStackExport, LearningResourcesExport} = require('../models/Learning-stacks-models')
 const User = require('../models/User')
+const { ObjectId } = require('mongodb');
 
 module.exports = {
     getLearningStackPage: async (req,res)=>{
@@ -10,12 +11,36 @@ module.exports = {
             // Pull user id
             const user = await User.findById(req.user.id).populate('learningStackArray');
 
+            console.log(`user: ${user}`)
+
+            const currentUsersID = req.session.passport.user
+
+            console.log(`currentUsersID: ${currentUsersID}`)
+
             // Pull logged in user's learning stack array
             const learningStackArray = user.learningStackArray
 
             // Pull specific learning stack
             const learningStack = await LearningStackExport.findById(req.params.id).populate('learningResourcesList')
+
+            console.log(`learningStackID: ${learningStack._id}`)
+
+            const userAssociatedToLearningStack = User.aggregate([
+                {
+                   $match: {
+                      _id: ObjectId(`${learningStack._id}`)
+                   }
+                },
+                {
+                   $project: {
+                      _id: 1
+                   }
+                }
+             ])
             
+            console.log(`userAssociatedToLearningStack: ${userAssociatedToLearningStack}`)
+            console.log(JSON.stringify(userAssociatedToLearningStack))
+
             // Learning stack details
             const learningStackName = learningStack.learningStackName
 
@@ -28,7 +53,10 @@ module.exports = {
                 learningStacks: learningStackArray,
                 learningStackName: learningStackName,
                 learningStackId: req.params.id,
-                learningResources: learningResourceList
+                learningResources: learningResourceList,
+                user: user,
+                currentUsersID: currentUsersID,
+                userAssociatedToLearningStack: userAssociatedToLearningStack
             })
 
         }catch(err){ 
